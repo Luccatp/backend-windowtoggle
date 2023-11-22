7b0f1a138a9a6cb1c2fd32e999909483f6382ab1
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-11-22 12:20:03 -0300

    feat: first POST endpoint

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,8 @@
 export async function GET(request: Request) {
   return new Response("Hello world!", { status: 200 });
 }
+
+export async function POST(request: Request) {
+  console.log(request.body);
+  return new Response("Hello world!", { status: 200 });
+}
